286945626c064f599daaf89eab487038ce9b4b00
Anish kanade <anishkanade45@gmail.com>
2024-11-18 15:38:03 -0500

    Final website version

---
+++ script.js
@@ -0,0 +1,4 @@
+import { faGithub, faFacebook, faTwitter, faTiktok, faLinkedin, faMailchimp } from '@fortawesome/free-brands-svg-icons';
+import (index.html)
+
+//<a href="#"><FontAwesomeIcon icon={faGithub} className="social-icon" /></a>
